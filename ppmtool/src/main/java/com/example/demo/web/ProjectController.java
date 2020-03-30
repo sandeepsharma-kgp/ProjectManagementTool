a5803e91b79feed5adf938a8ac1f0b313fcccdeb
sandeepsharma-kgp <sandeepsharma.iit@gmail.com>
2020-03-31 00:26:05 +0530

    error Map logic moved to service

--- ppmtool/src/main/java/com/example/demo/web/ProjectController.java
+++ ppmtool/src/main/java/com/example/demo/web/ProjectController.java
@@ -1,6 +1,7 @@
 package com.example.demo.web;
 
 import com.example.demo.domain.Project;
+import com.example.demo.services.MapValidationErrorService;
 import com.example.demo.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -22,15 +23,13 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
-        if(result.hasErrors()) {
-            Map<String, String> errorMap = new HashMap<>();
-            for(FieldError error : result.getFieldErrors()) {
-                errorMap.put(error.getField(), error.getDefaultMessage());
-            }
-            return new ResponseEntity<Map<String,String>>(errorMap, HttpStatus.BAD_REQUEST);
-        }
+        ResponseEntity<?> errorMap = mapValidationErrorService.mapValidationService(result);
+        if(errorMap!=null) return errorMap;
+
         Project project1 = projectService.saveOrUpdate(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
     }
