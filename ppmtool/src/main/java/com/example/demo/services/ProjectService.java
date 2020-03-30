cebc0b299c70a9df60ead64cb4c4fff5ee4b6ddb
sandeepsharma-kgp <sandeepsharma.iit@gmail.com>
2020-03-31 00:51:40 +0530

    handled db/entity exceptions

--- ppmtool/src/main/java/com/example/demo/services/ProjectService.java
+++ ppmtool/src/main/java/com/example/demo/services/ProjectService.java
@@ -1,6 +1,7 @@
 package com.example.demo.services;
 
 import com.example.demo.domain.Project;
+import com.example.demo.exceptions.ProjectIdException;
 import com.example.demo.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,6 +14,11 @@ public class ProjectService {
 
     public Project saveOrUpdate(Project project) {
         //Logics here
-        return projectRepository.save(project);
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception ex) {
+            throw new ProjectIdException("Project Id '" + project.getProjectIdentifier().toUpperCase() + "' already exists");
+        }
     }
 }
