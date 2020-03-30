f7c3f3e5d7a0c15b9cd6082cabb87b4bc2d634d4
sandeepsharma-kgp <sandeepsharma.iit@gmail.com>
2020-03-31 00:16:00 +0530

    error list is getting through api calls

--- ppmtool/src/main/java/com/example/demo/web/ProjectController.java
+++ ppmtool/src/main/java/com/example/demo/web/ProjectController.java
@@ -7,9 +7,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
@@ -20,8 +24,13 @@ public class ProjectController {
 
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
-        if(result.hasErrors())
-            return new ResponseEntity<String>("Invalid Project Object", HttpStatus.BAD_REQUEST);
+        if(result.hasErrors()) {
+            Map<String, String> errorMap = new HashMap<>();
+            for(FieldError error : result.getFieldErrors()) {
+                errorMap.put(error.getField(), error.getDefaultMessage());
+            }
+            return new ResponseEntity<Map<String,String>>(errorMap, HttpStatus.BAD_REQUEST);
+        }
         Project project1 = projectService.saveOrUpdate(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
     }
