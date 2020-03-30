dc3bdfe5bfed5e892e947ff4b9cec036d0d8ab2a
sandeepsharma-kgp <sandeepsharma.iit@gmail.com>
2020-03-31 00:02:40 +0530

    getting invalid object if some condition of request is failing

--- ppmtool/src/main/java/com/example/demo/web/ProjectController.java
+++ ppmtool/src/main/java/com/example/demo/web/ProjectController.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/project")
@@ -16,7 +19,9 @@ public class ProjectController {
     private ProjectService projectService;
 
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project) {
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
+        if(result.hasErrors())
+            return new ResponseEntity<String>("Invalid Project Object", HttpStatus.BAD_REQUEST);
         Project project1 = projectService.saveOrUpdate(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
     }
