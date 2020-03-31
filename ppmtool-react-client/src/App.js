071389a72855ea11488ec344d979d9aa3287a414
sandeepsharma-kgp <sandeepsharma.iit@gmail.com>
2020-03-31 17:27:51 +0530

    project and header components added

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Dashboard from './components/Dashboard';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Dashboard from "./components/Dashboard";
+import Header from "./components/Layout/Header";
 
 function App() {
   return (
     <div className="App">
-      <Dashboard/>      
+      <Header />
+      <Dashboard />
     </div>
   );
 }
--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -1,11 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import ProjectItem from "./Project/ProjectItem";
 
 class Dashboard extends Component {
-    render() {
-        return (
-            <h1>Welcome to the Dashboard!</h1>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h1>Welcome to the Dashboard!</h1>
+        <ProjectItem />
+      </div>
+    );
+  }
 }
 
 export default Dashboard;
